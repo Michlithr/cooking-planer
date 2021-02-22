f08af7344f77bccfd61d474d8fe970880e3f56b0
Michał Litwa <michal.litwa>
2021-02-22 16:14:19 +0100

    Initial commit with first ts support test.

---
+++ src/components/TsTesting.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button v-on:click="decrement">-</button>
+    {{ count }}
+    <button v-on:click="increment">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Counter extends Vue {
+  private count = 0;
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    this.count--;
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ title }}</h1>
+    <TsTesting />
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import TsTesting from "@/components/TsTesting.vue";
+
+@Options({
+  components: {
+    TsTesting
+  }
+})
+export default class About extends Vue {
+  private title = "This is an about page";
+}
+</script>
